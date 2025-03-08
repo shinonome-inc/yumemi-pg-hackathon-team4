7d27e5308c73c2ae921310179496c700244adb9f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-09 00:36:51 +0900

    main.tfの不要なコメントアウト解除

--- infra/main.tf
+++ infra/main.tf
@@ -1,22 +1,21 @@
-# FIXME: 409エラーが発生してしまうため、一旦コメントアウトしている。
-# resource "google_project" "default" {
-#   name            = "Yumemi-PG-Hackathon-Team4"
-#   provider        = google
-#   project_id      = var.project_id
-#   billing_account = var.billing_account
-# }
+resource "google_project" "default" {
+  name            = "Yumemi-PG-Hackathon-Team4"
+  provider        = google-beta
+  project_id      = var.project_id
+  billing_account = var.billing_account
+}
 
-# resource "google_project_service" "enable_services" {
-#   project = google_project.default.project_id
-#   service = each.key
+resource "google_project_service" "enable_services" {
+  project = google_project.default.project_id
+  service = each.key
 
-#   for_each = toset([
-#     "compute.googleapis.com",
-#     "cloudresourcemanager.googleapis.com",
-#     "iam.googleapis.com",
-#     "cloudbilling.googleapis.com",
-#     "billingbudgets.googleapis.com",
-#     "firebase.googleapis.com",
-#     "firebaserules.googleapis.com",
-#   ])
-# }
+  for_each = toset([
+    "compute.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "billingbudgets.googleapis.com",
+    "firebase.googleapis.com",
+    "firebaserules.googleapis.com",
+  ])
+}
