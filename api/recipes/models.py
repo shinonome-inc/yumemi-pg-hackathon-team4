fccb89c7ea56fa97ad6a94ffebe08c6b00e90532
morita-y9 <xiongxisentian@gmail.com>
2025-03-11 02:06:30 +0900

    model作成

--- api/recipes/models.py
+++ api/recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    title = models.CharField(max_length=255)
+    thumbnail_image_url = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tips = models.TextField(blank=True)
+    AI_comment = models.TextField(blank=True)
+    
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    ingredient_name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+    
+class Gathering_Steps(models.Model):
+    recipr = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='gathering_steps')
+    image_url = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+
+class Cooking_Steps(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='cooking_steps')
+    image_url = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+
+    
--- api/reviews/models.py
+++ api/reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+class Likes(models.Model):
+    review = models.OneToOneField('reviews.Review', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f'{self.user} likes {self.review}'
+    
+class Comment(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey('reviews.Review', on_delete=models.CASCADE, related_name='comments')
+    content_text = models.TextField()
+    image_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
