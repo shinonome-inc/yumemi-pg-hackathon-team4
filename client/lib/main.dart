1b98dda8e66694bc6158ac6e7c404bf60934ef0c
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-10 21:18:18 +0900

    flutter_dotenvを追加

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:client/firebase_options.dart';
 import 'package:client/sample_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
