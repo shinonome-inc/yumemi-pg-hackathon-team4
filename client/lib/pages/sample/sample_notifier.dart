d2a7e0e778d89455e0375c2a89dd77bd268a4b39
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-13 21:19:38 +0900

    SamplePageにFirebaseの動作確認用のコードを追加

--- client/lib/pages/sample/sample_notifier.dart
+++ client/lib/pages/sample/sample_notifier.dart
@@ -1,4 +1,7 @@
+import 'package:client/models/recipe.dart';
+import 'package:client/models/user.dart';
 import 'package:client/pages/sample/sample_state.dart';
+import 'package:client/services/cook_wild_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'sample_notifier.g.dart';
@@ -16,5 +19,27 @@ class SampleNotifier extends _$SampleNotifier {
 
   void setIsLoading({required bool isLoading}) {
     state = state.copyWith(isLoading: isLoading);
+  }
+
+  void setRecipes({required List<Recipe> recipes}) {
+    state = state.copyWith(recipes: recipes);
+  }
+
+  void setUsers({required List<User> users}) {
+    state = state.copyWith(users: users);
+  }
+
+  Future<void> fetchRecipesAndUsers() async {
+    setIsLoading(isLoading: true);
+    try {
+      final recipes = await CookWildService.instance.getRecipes();
+      final users = await CookWildService.instance.getUsers();
+      setRecipes(recipes: recipes);
+      setUsers(users: users);
+    } catch (e) {
+      throw Exception('Failed to fetch recipes or users: $e');
+    } finally {
+      setIsLoading(isLoading: false);
+    }
   }
 }
--- client/lib/pages/sample/sample_page.dart
+++ client/lib/pages/sample/sample_page.dart
@@ -1,3 +1,8 @@
+import 'package:client/pages/sample/sample_notifier.dart';
+import 'package:client/pages/sample/sample_recipe_view.dart';
+import 'package:client/pages/sample/sample_sign_in_view.dart';
+import 'package:client/pages/sample/sample_sign_up_view.dart';
+import 'package:client/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,8 +14,83 @@ class SamplePage extends ConsumerStatefulWidget {
 }
 
 class _TopPageState extends ConsumerState<SamplePage> {
+  Future<void> _onPressedSignOut() async {
+    await AuthService.instance.signOut();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      await ref.read(sampleNotifierProvider.notifier).fetchRecipesAndUsers();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final state = ref.watch(sampleNotifierProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Page'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+            child: Column(
+              children: [
+                const ExpansionTile(
+                  title: Text('Sign Up Sample'),
+                  children: <Widget>[
+                    SampleSignUpView(),
+                  ],
+                ),
+                const ExpansionTile(
+                  title: Text('Sign In Sample'),
+                  children: <Widget>[
+                    SampleSignInView(),
+                  ],
+                ),
+                ExpansionTile(
+                  title: const Text('Sign Out Sample'),
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: _onPressedSignOut,
+                      child: const Text('Sign Out'),
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  title: const Text('Recipe Sample'),
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(left: 32),
+                      child: SampleRecipeView(state: state),
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  title: const Text('User Sample'),
+                  children: <Widget>[
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: state.users.length,
+                      itemBuilder: (context, index) {
+                        final user = state.users.elementAt(index);
+                        return ListTile(
+                          title: Text(user.name),
+                          subtitle: Text('@${user.id}'),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 64),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
