0e153068021c0b37bbc1b7e14d3ee33f65e00583
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-14 13:18:27 +0900

    CI/CDの不具合を修正するためにbuild_runnerの生成ファイルをGitの差分に追加

---
+++ client/lib/models/gathering_steps.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gathering_steps.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$GatheringStepsImpl _$$GatheringStepsImplFromJson(Map<String, dynamic> json) =>
+    _$GatheringStepsImpl(
+      id: json['id'] as String,
+      imageUrl: json['imageUrl'] as String?,
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$$GatheringStepsImplToJson(
+        _$GatheringStepsImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'imageUrl': instance.imageUrl,
+      'description': instance.description,
+    };
---
+++ client/lib/pages/recipe_detail/recipe_detail_notifier.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipe_detail_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$recipeDetailNotifierHash() =>
+    r'4ea753ff635d34355dccb46ef2138966bc69b77c';
+
+/// stateを管理し、状態の変更を行うクラス。
+///
+/// 状態を保持し、状態を更新するためのメソッドを提供する。
+///
+/// Copied from [RecipeDetailNotifier].
+@ProviderFor(RecipeDetailNotifier)
+final recipeDetailNotifierProvider = AutoDisposeNotifierProvider<
+    RecipeDetailNotifier, RecipeDetailState>.internal(
+  RecipeDetailNotifier.new,
+  name: r'recipeDetailNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$recipeDetailNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$RecipeDetailNotifier = AutoDisposeNotifier<RecipeDetailState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
