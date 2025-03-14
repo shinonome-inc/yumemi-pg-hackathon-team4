0b9e6ef0ead5cf84bb5f305e429e7b43fdbd21d4
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-03-14 15:25:58 +0900

    3/14 recipes

--- api/recipes/serializers.py
+++ api/recipes/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import Recipe
+from .models import Recipe, Ingredient, GatheringStep, CookingStep
+
+
+#class RecipeSerializer(serializers.ModelSerializer):
+    #class Meta:
+        #model = Recipe
+        #fields = ['id','title','thumbnail_image_url','description','tips','AI_comment','ingredients','cooking_steps','gathering_steps','created_at','updated_at']
+        #read_only_fields = ["id",'AI_comment',"created_at",'updated_at']
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = 'all'
+
+class GatheringStepSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GatheringStep
+        fields = 'all'
+
+class CookingStepSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CookingStep
+        fields = 'all'
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    gathering_steps = GatheringStepSerializer(many=True, read_only=True)
+    cooking_steps = CookingStepSerializer(many=True, read_only=True)
+
     class Meta:
         model = Recipe
-        fields = ['id','title','thumbnail_image_urls','description','tips','AI_comment','ingredients','cooking_steps','gathering_steps','created_at','updated_at']
-        read_only_fields = ["id",'AI_comment',"created_at",'updated_at']
+        fields = 'all'
--- api/recipes/views.py
+++ api/recipes/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from .models import Recipe, Ingredient, GatheringStep, CookingStep
+from .serializers import RecipeSerializer, IngredientSerializer, GatheringStepSerializer, CookingStepSerializer
 from rest_framework.permissions import AllowAny
 
-from .models import Recipe
-from .serializers import RecipeSerializer
-
-# Create your views here.
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
-    serializer_class = RecipesSerializer
+    serializer_class = RecipeSerializer
+    authentication_classes = []  # 認証なし
+    permission_classes = [AllowAny]  # 誰でもアクセス可能
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    authentication_classes = []  # 認証なし
+    permission_classes = [AllowAny]  # 誰でもアクセス可能
+
+class GatheringStepViewSet(viewsets.ModelViewSet):
+    queryset = GatheringStep.objects.all()
+    serializer_class = GatheringStepSerializer
+    authentication_classes = []  # 認証なし
+    permission_classes = [AllowAny]  # 誰でもアクセス可能
+
+class CookingStepViewSet(viewsets.ModelViewSet):
+    queryset = CookingStep.objects.all()
+    serializer_class = CookingStepSerializer
     authentication_classes = []  # 認証なし
     permission_classes = [AllowAny]  # 誰でもアクセス可能
