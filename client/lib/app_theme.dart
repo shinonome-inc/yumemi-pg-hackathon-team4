3f128df384ddd8e37fda7e9b9d6c822e5863fe68
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-16 23:41:55 +0900

    appThemeを追加

---
+++ client/lib/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+/// アプリ内共通のThemeを定義。
+ThemeData appTheme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  );
+}
