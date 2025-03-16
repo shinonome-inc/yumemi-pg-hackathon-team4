71bcde652665b2e375bdd2648657aba89b50e294
haru <hinagoro5656@gmail.com>
2025-03-17 01:03:01 +0900

    ログインページのおおまかなUI作成

--- client/lib/pages/top/top_page.dart
+++ client/lib/pages/top/top_page.dart
@@ -1,3 +1,6 @@
+import 'package:client/constants/app_colors.dart';
+import 'package:client/extensions/build_context_extension.dart';
+import 'package:client/extensions/text_theme_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,6 +14,149 @@ class TopPage extends ConsumerStatefulWidget {
 class _TopPageState extends ConsumerState<TopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              constraints: const BoxConstraints(
+                maxWidth: 360,
+              ),
+              height: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'クックワイルド',
+                        style: context.textTheme.titleLargeBold?.copyWith(
+                          color: AppColors.green1,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 48),
+                  Text(
+                    'ログイン',
+                    style: context.textTheme.titleLargeBold?.copyWith(
+                      color: AppColors.gray1,
+                    ),
+                  ),
+                  const SizedBox(height: 36),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'メールアドレス',
+                        style: context.textTheme.bodyMedium?.copyWith(
+                          color: AppColors.gray1,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'example@email.com',
+                          hintStyle: TextStyle(
+                            color: AppColors.gray2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'パスワード',
+                        style: context.textTheme.bodyMedium?.copyWith(
+                          color: AppColors.gray1,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'password',
+                          hintStyle: TextStyle(
+                            color: AppColors.gray2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: FilledButton(
+                      onPressed: () {},
+                      child: const Text('ログイン'),
+                      style: FilledButton.styleFrom(
+                        backgroundColor: AppColors.green1,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: AppColors.gray3,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Text(
+                          'または',
+                          style: TextStyle(color: AppColors.gray2),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: AppColors.gray3,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Googleでログイン'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        '新規登録はこちら',
+                        style: TextStyle(
+                          color: AppColors.green1,
+                          decoration: TextDecoration.underline,
+                          decorationColor: AppColors.green1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
