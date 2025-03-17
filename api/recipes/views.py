ac970673bdcf18448eb4daa25842050b1bed8f32
morita-y9 <xiongxisentian@gmail.com>
2025-03-18 04:04:39 +0900

    レシピ関連

--- api/recipes/views.py
+++ api/recipes/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Recipe, Ingredient, GatheringStep, CookingStep
+from .serializers import RecipeSerializer, IngredientSerializer, GatheringStepSerializer, CookingStepSerializer
+from rest_framework.permissions import AllowAny
+
+from authentication.authentication import FirebaseAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    authentication_classes = [FirebaseAuthentication]  # Firebase 認証を適用
+    permission_classes = [IsAuthenticated]
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    authentication_classes = [FirebaseAuthentication]  # Firebase 認証を適用
+    permission_classes = [IsAuthenticated]
+
+class GatheringStepViewSet(viewsets.ModelViewSet):
+    queryset = GatheringStep.objects.all()
+    serializer_class = GatheringStepSerializer
+    authentication_classes = [FirebaseAuthentication]  # Firebase 認証を適用
+    permission_classes = [IsAuthenticated]
+
+class CookingStepViewSet(viewsets.ModelViewSet):
+    queryset = CookingStep.objects.all()
+    serializer_class = CookingStepSerializer
+    authentication_classes = [FirebaseAuthentication]  # Firebase 認証を適用
+    permission_classes = [IsAuthenticated]
 
 
-# Create your views here.
-class RecipeViewSet:
-    pass
+
+
+
