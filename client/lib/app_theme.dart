158de9480c9212d03e6ce69a451c934321dd1806
haru <hinagoro5656@gmail.com>
2025-03-17 19:51:33 +0900

    seedColorをgreen1に変更

--- client/lib/app_theme.dart
+++ client/lib/app_theme.dart
@@ -1,10 +1,11 @@
+import 'package:client/constants/app_colors.dart';
 import 'package:client/constants/font_families.dart';
 import 'package:flutter/material.dart';
 
 /// アプリ内共通のThemeを定義。
 ThemeData appTheme() {
   return ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    colorScheme: ColorScheme.fromSeed(seedColor: AppColors.green1),
     fontFamily: FontFamilies.notoSansJP,
   );
 }
