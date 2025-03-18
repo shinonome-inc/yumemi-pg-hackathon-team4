959d1ddd344beb6b550a96c758e5bcf0dcf9e249
Tomu <ttomfjkw@gmail.com>
2025-03-18 15:58:11 +0900

    TabBarのコメント枠作成

---
+++ client/lib/pages/recipe_detail/recipe_detail_recipe_comments_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipeCommentsComponent extends StatelessWidget {
+  const RecipeCommentsComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('コメントの内容'));
+  }
+}
