9fbd6472de7e7b1c628201afa589624e3c0d27cb
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-26 01:00:15 +0900

    StateにisObscurePasswordを追加

--- client/lib/pages/top/top_state.dart
+++ client/lib/pages/top/top_state.dart
@@ -8,10 +8,12 @@ part 'top_state.freezed.dart';
 class TopState with _$TopState {
   const factory TopState({
     required bool isLoading,
+    required bool isObscurePassword,
   }) = _TopState;
 }
 
 /// Stateの初期値。
 const TopState initialTopState = TopState(
   isLoading: false,
+  isObscurePassword: true,
 );
