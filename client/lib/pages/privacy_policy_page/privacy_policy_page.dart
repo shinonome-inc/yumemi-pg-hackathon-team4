6bb4bc34b3e5d6700c06c7f663322101969a7d7e
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-25 23:16:37 +0900

    PrivacyPolicyPageを追加

---
+++ client/lib/pages/privacy_policy_page/privacy_policy_page.dart
@@ -0,0 +1,41 @@
+import 'package:client/components/app_content_view.dart';
+import 'package:client/enums/app_contents_type.dart';
+import 'package:client/extensions/app_content_extension.dart';
+import 'package:client/repository/firestore_repository.dart';
+import 'package:flutter/material.dart';
+
+class PrivacyPolicyPage extends StatefulWidget {
+  const PrivacyPolicyPage({super.key});
+
+  @override
+  State<PrivacyPolicyPage> createState() => _PrivacyPolicyPageState();
+}
+
+class _PrivacyPolicyPageState extends State<PrivacyPolicyPage> {
+  String _privacyPolicyText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      final content = await FirestoreRepository.instance.getAppContentByType(
+        AppContentType.privacyPolicy,
+      );
+      setState(() {
+        _privacyPolicyText = content.displayText.replaceAll(r'\n', '\n');
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('プライバシーポリシー'),
+      ),
+      body: SafeArea(
+        child: AppContentView(contentText: _privacyPolicyText),
+      ),
+    );
+  }
+}
