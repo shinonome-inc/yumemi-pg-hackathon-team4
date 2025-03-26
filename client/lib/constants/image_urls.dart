19cf7435ca9e5dd00efa518aff837b8fd1f4abb6
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-26 18:59:00 +0900

    ImageUrlsクラスを追加

---
+++ client/lib/constants/image_urls.dart
@@ -0,0 +1,8 @@
+/// 画像アドレスの文字列を扱う定数クラス。
+class ImageUrls {
+  ImageUrls._();
+
+  /// ユーザーの初期アイコン画像。
+  static const defaultUserIcon =
+      'https://knsoza1.com/wp-content/uploads/2020/07/70b3dd52350bf605f1bb4078ef79c9b9.png';
+}
