2268015902ed75a9672d26e97f94c758b3bc76d2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-26 18:27:48 +0900

    SplashPageを追加

---
+++ client/lib/pages/splash_page/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SvgPicture.asset(
+            'assets/images/Splash.svg',
+            width: 320,
+          ),
+        ),
+      ),
+    );
+  }
+}
