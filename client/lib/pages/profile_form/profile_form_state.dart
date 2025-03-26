a1aed7ef26cd139c49aa265470a62f771cf4756a
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-26 18:57:41 +0900

    UserProfileEditPageの繋ぎ込み実装

--- client/lib/pages/profile_form/profile_form_state.dart
+++ client/lib/pages/profile_form/profile_form_state.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'profile_form_state.freezed.dart';
@@ -8,10 +10,12 @@ part 'profile_form_state.freezed.dart';
 class ProfileFormState with _$ProfileFormState {
   const factory ProfileFormState({
     required bool isLoading,
+    required File? selectedImage,
   }) = _ProfileFormState;
 }
 
 /// Stateの初期値。
 const ProfileFormState initialProfileFormState = ProfileFormState(
   isLoading: false,
+  selectedImage: null,
 );
